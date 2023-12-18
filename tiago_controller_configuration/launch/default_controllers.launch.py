740e0bb629583e0aa276ba1adbb506f84338f83c
Noel Jimenez <noel.jimenez@pal-robotics.com>
2023-12-18 14:07:22 +0000

    Rebase from 'upstream'

--- tiago_controller_configuration/launch/default_controllers.launch.py
+++ tiago_controller_configuration/launch/default_controllers.launch.py
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.conditions import LaunchConfigurationNotEquals
-from launch.substitutions import LaunchConfiguration
+from launch.actions import OpaqueFunction
+from launch.conditions import IfCondition, LaunchConfigurationNotEquals
+from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_pal.arg_utils import read_launch_argument
 from launch_pal.include_utils import include_launch_py_description
-from launch_pal.robot_utils import get_arm, get_end_effector, get_robot_name
+from launch_pal.robot_utils import get_arm, get_end_effector, get_ft_sensor, get_robot_name
 
 
 def declare_robot_args(context, *args, **kwargs):
@@ -26,30 +26,35 @@ def declare_robot_args(context, *args, **kwargs):
     robot_name = read_launch_argument('robot_name', context)
 
     return [get_arm(robot_name),
-            get_end_effector(robot_name)]
+            get_end_effector(robot_name),
+            get_ft_sensor(robot_name)]
 
 
 def launch_end_effector_controller(context, *args, **kwargs):
 
-    if (read_launch_argument('end_effector', context) == 'no-end-effector'):
+    end_effector_param = read_launch_argument('end_effector', context)
+    if (end_effector_param == 'no-end-effector'):
         return []
 
-    end_effector_launcher = read_launch_argument('end_effector_controller_launch', context)
+    # Temporal fix. This will be removed.
+    if (end_effector_param == 'pal-hey5'):
+        hey_5_controller_launch = include_launch_py_description(
+            'tiago_controller_configuration',
+            ['launch', end_effector_param + '_controller.launch.py'],
+            condition=LaunchConfigurationNotEquals('arm', 'no-arm'))
+        return [hey_5_controller_launch]
+
+    end_effector = end_effector_param.replace('-', '_')
+
     end_effector_controller_launch = include_launch_py_description(
-        'tiago_controller_configuration',
-        ['launch', end_effector_launcher],
+        end_effector + '_controller_configuration',
+        ['launch', end_effector + '_controller.launch.py'],
         condition=LaunchConfigurationNotEquals('arm', 'no-arm'))
 
     return [end_effector_controller_launch]
 
 
 def generate_launch_description():
-
-    end_effector_controller = DeclareLaunchArgument(
-        'end_effector_controller_launch',
-        default_value=[LaunchConfiguration('end_effector'), '_controller.launch.py'],
-        description='end effector controller launch file')
-
     mobile_base_controller_launch = include_launch_py_description(
         'tiago_controller_configuration',
         ['launch', 'mobile_base_controller.launch.py'])
@@ -71,17 +76,27 @@ def generate_launch_description():
         ['launch', 'arm_controller.launch.py'],
         condition=LaunchConfigurationNotEquals('arm', 'no-arm'))
 
+    ft_sensor_controller_launch = include_launch_py_description(
+        'tiago_controller_configuration',
+        ['launch', 'ft_sensor_controller.launch.py'],
+        condition=IfCondition(
+            PythonExpression(
+                ["'", LaunchConfiguration('arm'), "' != 'no-arm' and '",
+                 LaunchConfiguration('ft_sensor'), "' != 'no-ft-sensor'"]
+            )
+        ))
+
     ld = LaunchDescription()
 
     ld.add_action(get_robot_name('tiago'))
     ld.add_action(OpaqueFunction(function=declare_robot_args))
-    ld.add_action(end_effector_controller)
 
     ld.add_action(mobile_base_controller_launch)
     ld.add_action(joint_state_broadcaster_launch)
     ld.add_action(torso_controller_launch)
     ld.add_action(head_controller_launch)
     ld.add_action(arm_controller_launch)
+    ld.add_action(ft_sensor_controller_launch)
     ld.add_action(OpaqueFunction(function=launch_end_effector_controller))
 
     return ld
